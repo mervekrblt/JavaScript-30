27c819715a69878779d610c280f44ef427d5ec13
Merve Karabulut <mervekarabulut@lonca.works>
2020-08-30 16:57:59 +0300

    Play sound when click the button also & change styling

--- JS Drum Kit/script.js
+++ JS Drum Kit/script.js
@@ -1,7 +1,43 @@
-window.addEventListener('keydown', function(e){
+function playSound(e) {
     let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`) //choose audio element which is corresponding to button which I press
+
+    let key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
     console.log(audio)
-    if(!audio)return ; //stop function and return 
+
+    if (!audio) return; //stop function and return, if I dont add this code, stop running when I press the button that dont have audio
+
+    audio.currentTime = 0; //returns the current position (in seconds) of the audio playback
+
     audio.play();
+
+    key.classList.add('playing') //dont write .playing, you write classlist already
+}
+
+function clickSound() {
     
-})
+    const audio = document.querySelector(`audio[data-key="${this.dataset.key}"]`);
+
+    if (!audio) return;
+
+    audio.currentTime = 0;
+    audio.play();
+
+    this.classList.add('playing');
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== 'transform')
+        this.classList.remove('playing')
+    return
+}
+
+const keys = document.querySelectorAll('.key')
+keys.forEach(key => key.addEventListener('transitionend', removeTransition))
+keys.forEach((key) => key.addEventListener('click', clickSound));
+
+window.addEventListener('keydown', playSound)
+window.addEventListener('click', playSound)
+
+
+
+
