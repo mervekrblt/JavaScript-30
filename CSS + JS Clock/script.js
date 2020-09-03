0a4d2b7b382b7332bcd3ae80f1d9ea6ee46d7a6d
Merve Karabulut <mervekarabulut@lonca.works>
2020-09-03 21:24:12 +0300

    Set clock's degree for sec, min and hour

--- CSS + JS Clock/script.js
+++ CSS + JS Clock/script.js
@@ -1,7 +1,22 @@
+const secondHand = document.querySelector('.sec')
+const minHand = document.querySelector('.min')
+const hourHand = document.querySelector('.hour')
+
 function setDate(){
     const now = new Date()
     const sec = now.getSeconds()
-    console.log(sec)
+    const secDegree = (((sec/60)*360)+ 90)  //default in CSS it is written 90deg
+    secondHand.style.transform = `rotate(${secDegree}deg)`
+
+
+    const min = now.getMinutes()
+    const minDegree = (((min/60)*360) +90)
+    minHand.style.transform = `rotate(${minDegree}deg)`
+
+    const hour = now.getHours()
+    const hourDegree = (hour*30)+90
+    hourHand.style.transform = `rotate(${hourDegree}deg)`
+    console.log(hourDegree)
 }
 
 setInterval(setDate, 1000)
