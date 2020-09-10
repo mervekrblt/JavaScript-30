97741f39869ac77a0aa3a41923787823b2c98a67
Merve Karabulut <mervekarabulut@lonca.works>
2020-09-10 15:02:22 +0300

    Change blur, spacing and color when click buttons

--- Playing with CSS Variables and JS/script.js
+++ Playing with CSS Variables and JS/script.js
@@ -1,3 +1,14 @@
 $('#btn').on('click', function() {
     $('#img').attr('src', $('#url').val());
   });
+
+const inputs = document.querySelectorAll('.controls input')
+console.log(inputs)
+
+function handleUpdate(){
+  const suffix = this.dataset.sizing || ''
+  document.documentElement.style.setProperty(`--${this.name}`, this.value + `${suffix}`)
+}
+
+inputs.forEach(input => input.addEventListener('change', handleUpdate))
+inputs.forEach(input => input.addEventListener('mousemove', handleUpdate))
