06d894c5dad27b09372839ae203fd50d35023fc5
Merve Karabulut <mervekarabulut@lonca.works>
2020-09-20 17:48:34 +0300

    When click image, show up and down panels with transition

--- Flex Panels Image Gallery/script.js
+++ Flex Panels Image Gallery/script.js
@@ -2,6 +2,7 @@ const panels = document.querySelectorAll('.panel')
 
 function toggleOpen(){
     this.classList.toggle('open') //dont write .open
+    this.classList.toggle('open-active')  // I dont understand that why he write another function for this ?????
 }
 
 panels.forEach(panel => panel.addEventListener('click', toggleOpen))
