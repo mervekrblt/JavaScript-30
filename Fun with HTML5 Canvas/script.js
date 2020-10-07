a47effeb1e9ee1c4706b499e9f401d542068019b
Merve Karabulut <mervekarabulut@lonca.works>
2020-10-07 12:51:16 +0300

    Set a color of line from color palette & Add a clear button for redrawing

--- Fun with HTML5 Canvas/script.js
+++ Fun with HTML5 Canvas/script.js
@@ -4,7 +4,16 @@ const ctx = canvas.getContext('2d')
 canvas.width = window.innerWidth
 canvas.height = window.innerHeight
 
-ctx.strokeStyle = "black"
+//add from stack overflow
+var color = "rgb(255,0,0)";
+
+function change(){
+    color = this.value;
+}
+
+document.getElementById("color").onchange = change;
+//add from stack overflow
+
 ctx.lineJoin = 'round'
 ctx.lineCap = 'round'
 ctx.lineWidth = 5
@@ -12,12 +21,10 @@ ctx.lineWidth = 5
 let isDrawing = false
 let lastX = 0
 let lastY = 0
-let hue = 0
 
 function draw(e){
     if(!isDrawing) return
-    
-    ctx.strokeStyle= `hsl(${hue}, 100%, 50%)`
+    ctx.strokeStyle = color //add from stack overflow
     ctx.beginPath()
     //start from
     ctx.moveTo(lastX, lastY)
@@ -26,7 +33,6 @@ function draw(e){
     ctx.stroke()
     lastX = e.offsetX
     lastY = e.offsetY
-    hue++
 }
 
 canvas.addEventListener('mousemove', draw)
@@ -37,3 +43,9 @@ canvas.addEventListener('mousedown', (e) => {
 })
 canvas.addEventListener('mouseup', () => isDrawing=false)
 canvas.addEventListener('mouseout', () => isDrawing= false)
+
+const erase = document.querySelector('#erase')
+
+erase.addEventListener('click', function(){
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+})
