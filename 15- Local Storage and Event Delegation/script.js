22ee9500425212556961805a85a28f019c00ea96
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2020-11-01 11:46:42 +0300

    Show input value in the list when user submit the input

--- 15- Local Storage and Event Delegation/script.js
+++ 15- Local Storage and Event Delegation/script.js
@@ -1,3 +1,30 @@
 const addItems = document.querySelector('.add-items') 
 const itemsList = document.querySelector('.plates')
 const items = []
+
+function addItem(e){
+    e.preventDefault()
+    const text = (this.querySelector("[name=item]")).value
+    const item = {
+        text :text,
+        done:false
+    }
+    items.push(item)
+
+    populateList(items, itemsList)
+    this.reset()
+}
+
+function populateList(plates=[], platesList){ // first var take an array, second var is where plates to put into 
+
+    platesList.innerHTML = plates.map((plate,i) => {
+        return `
+        <li>
+        <input type="checkbox" name="" id="">
+        <label for="" name="item">${plate.text}</label>
+        </li>
+        `
+    }).join('')
+}
+
+addItems.addEventListener('submit', addItem)
