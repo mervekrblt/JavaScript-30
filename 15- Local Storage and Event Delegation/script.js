6a1b64712a89127b8aeedfd81d33943d95869889
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2020-11-02 13:14:30 +0300

    Add items into local storage. When user refreshs page, get items from local storage and show on the page

--- 15- Local Storage and Event Delegation/script.js
+++ 15- Local Storage and Event Delegation/script.js
@@ -1,6 +1,6 @@
 const addItems = document.querySelector('.add-items') 
 const itemsList = document.querySelector('.plates')
-const items = []
+const items = JSON.parse(localStorage.getItem('items')) || []
 
 function addItem(e){
     e.preventDefault()
@@ -12,6 +12,7 @@ function addItem(e){
     items.push(item)
 
     populateList(items, itemsList)
+    localStorage.setItem('items', JSON.stringify(items))
     this.reset()
 }
 
@@ -20,11 +21,13 @@ function populateList(plates=[], platesList){ // first var take an array, second
     platesList.innerHTML = plates.map((plate,i) => {
         return `
         <li>
-        <input type="checkbox" name="" id="">
-        <label for="" name="item">${plate.text}</label>
+        <input type="checkbox" data-index='${i}' id="item${i}" ${plate.done ? 'checked' : ''}>
+        <label for="item${i}" name="item">${plate.text}</label>
         </li>
         `
     }).join('')
 }
 
 addItems.addEventListener('submit', addItem)
+
+populateList(items, itemsList)
