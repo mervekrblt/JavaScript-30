a81eccc37edcc70fb54178118c14522152f85ffa
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2020-11-06 20:46:15 +0300

    Add 16th exercise

--- 16- Mouse Move Effect/script.js
+++ 16- Mouse Move Effect/script.js
@@ -1,8 +1,29 @@
 const div = document.querySelector('div')
-const eyes = document.querySelector('.eyes')
+const h1 = document.querySelector('h1')
+const walk = 200
 
-const shadow = (e) =>{
- console.log(e)
+const shadow = (e) => {
+    const { offsetHeight: height, offsetWidth: width} = div
+
+    let {offsetX: x, offsetY: y} = e
+
+    if (this !== e.target) {
+      x = x + e.target.offsetLeft
+      y = y + e.target.offsetTop
+    }
+    
+
+
+
+const xWalk = Math.round((x / width * walk) - (walk / 2));
+const yWalk = Math.round((y / height * walk) - (walk / 2));
+
+    h1.style.textShadow = `
+      ${xWalk}px ${yWalk}px 0 rgba(255,0,255,0.7),
+      ${xWalk * -1}px ${yWalk}px 0 rgba(0,255,255,0.7),
+      ${yWalk}px ${xWalk* -1}px 0 rgba(0,255,0,0.7),
+      ${yWalk * -1}px ${xWalk}px 0 rgba(0,0,255,0.7)
+    `;
 }
 
 div.addEventListener('mousemove', shadow)
