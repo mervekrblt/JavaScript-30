a3e3b7ad02ee0d242a9724bc8d1ee5ea6aa9dc23
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2020-11-11 13:09:48 +0300

    Display webcam player on the screen

--- 19-Unreal Webcam Fun/script.js
+++ 19-Unreal Webcam Fun/script.js
@@ -3,3 +3,31 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+
+function getVideo() {
+    navigator.mediaDevices
+      .getUserMedia({ video: true, audio: false })
+      .then((localMediaStream) => {
+        video.srcObject = localMediaStream;
+        video.play();
+      })
+      .catch((err) => {
+        console.error(`OH NO!!!`, err);
+      });
+  }
+
+  const paintToCanvas =() =>{
+      const {videoHeight: height, videoWidth:width} = video //ES6 feature
+      canvas.height = height
+      canvas.width = width
+
+      //take and put photo of video into canvas every 16 msc
+      setInterval(() =>{
+          ctx.drawImage(video, 0, 0, width, height)
+      },16)
+  }
+
+
+getVideo()
+video.addEventListener('canplay', paintToCanvas)
