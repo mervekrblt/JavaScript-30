61b6b7d6fa149032bf14706279c601ceb20121bf
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2020-11-12 15:26:27 +0300

    Take photo and display the photo on canvas element. Also user can dowloand images

--- 19-Unreal Webcam Fun/script.js
+++ 19-Unreal Webcam Fun/script.js
@@ -6,27 +6,48 @@ const snap = document.querySelector('.snap');
 
 
 function getVideo() {
-    navigator.mediaDevices
-      .getUserMedia({ video: true, audio: false })
-      .then((localMediaStream) => {
-        video.srcObject = localMediaStream;
-        video.play();
-      })
-      .catch((err) => {
-        console.error(`OH NO!!!`, err);
-      });
-  }
+  navigator.mediaDevices
+    .getUserMedia({
+      video: true,
+      audio: false
+    })
+    .then((localMediaStream) => {
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch((err) => {
+      console.error(`OH NO!!!`, err);
+    });
+}
 
-  const paintToCanvas =() =>{
-      const {videoHeight: height, videoWidth:width} = video //ES6 feature
-      canvas.height = height
-      canvas.width = width
+const paintToCanvas = () => {
+  const {
+    videoHeight: height,
+    videoWidth: width
+  } = video //ES6 feature
+  canvas.height = height
+  canvas.width = width
 
-      //take and put photo of video into canvas every 16 msc
-      setInterval(() =>{
-          ctx.drawImage(video, 0, 0, width, height)
-      },16)
-  }
+  //take and put photo of video into canvas every 16 msc
+  setInterval(() => {
+    ctx.drawImage(video, 0, 0, width, height)
+  }, 16)
+}
+
+const takePhoto = () =>{
+  snap.currentTime = 0
+  snap.play()
+
+  //take data out of canvas
+  const data = canvas.toDataURL("image/jpeg")
+  console.log(data)
+  const link = document.createElement("a")
+  link.href=data
+  link.setAttribute('dowloand', 'perfect')
+  link.innerHTML = `<img src="${data}" >`
+  strip.insertBefore(link, strip.firstChild)
+  console.log(link)
+}
 
 
 getVideo()
