fea6107406be756e3d6341ca7a9da1b1645a977b
Merve Karabulut <63372370+mervekrblt@users.noreply.github.com>
2020-11-23 14:45:41 +0300

    When user click the link, show dropdown menu (fix background later)

---
+++ 26-Dropdown/script.js
@@ -0,0 +1,33 @@
+const triggers = document.querySelectorAll('.cool > li')
+const background = document.querySelector('.dropdownBackground')
+const nav = document.querySelector('.top')
+
+//If I define dropdown out of handleEnter function, and use querySelector... It gives just first dropdown
+//const dropdown = document.querySelector('.dropdown')
+
+//When mouse enter the link
+function handleEnter(){
+    this.classList.add('trigger-enter')
+    setTimeout(()=> this.classList.add('trigger-enter-active'),150)
+
+    //add background
+    background.classList.add('open')
+
+    //select the link which has mouseenter
+    const dropdown = this.querySelector('.dropdown') 
+    // get coordinates of dropdown
+    const dropdownCoords = dropdown.getBoundingClientRect()
+    background.style.setProperty('height', `${dropdownCoords.height}px`)
+    background.style.setProperty('width', `${dropdownCoords.width}px`)
+    // continue here
+}
+
+//When mouse leave the link
+function handleLeave(){
+    this.classList.remove('trigger-enter', 'trigger-enter-active')
+    background.classList.remove('open')
+}
+
+//add listener to triggers
+triggers.forEach(i => i.addEventListener('click', handleEnter))
+triggers.forEach(i => i.addEventListener('mouseleave', handleLeave))
